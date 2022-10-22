e23312645d8258703dbba36073e61b6a4c92aa7d
zor07 <anzor.karmov@gmail.com>
2022-10-22 20:38:36 +0300

    Header.tsx

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+
+type HeaderPropsType = {
+    isAuth: boolean
+    username: string
+    logout: () => void
+}
+
+const Header: React.FC<HeaderPropsType> = ({isAuth, username, logout}) => {
+    return (
+        <>
+            <div className="logo">
+                NoFapp
+            </div>
+            <Navbar isAuth={isAuth} username={username} logout={logout}/>
+        </>
+    )
+}
+
+export default Header

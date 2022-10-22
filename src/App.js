6c452ecd0607045775aef9d505556d325bf57887
zor07 <anzor.karmov@gmail.com>
2022-10-22 20:36:22 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./components/Login/Login";
+import React, {Component} from "react";
+import {connect, Provider} from "react-redux";
+import {compose} from "redux";
+import {initializeApp} from "./redux/app-reducer.ts";
+import store from "./redux/redux-store.ts";
+import {Layout} from "antd";
+import 'antd/dist/antd.css';
+import HeaderContainer from "./components/Header/HeaderContainer";
+import NotebookListContainer from "./components/Notebook/NotebookListContainer";
+import NoteListContainer from "./components/Note/NoteListContainer";
+import NoteEditorContainer from "./components/Note/NoteEditorContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    this.props.initializeApp()
+  }
+
+  render() {
+    const {Header, Content} = Layout;
+    return (
+        <Layout>
+          <Header className="header" theme="light">
+            <HeaderContainer/>
+          </Header>
+          <Layout>
+            <Layout style={{padding: '12px 12px 12px'}}>
+              <Content
+                  className="site-layout-background"
+                  style={{
+                    padding: '0 50px',
+                    margin: 0,
+                    minHeight: 280,
+                  }}>
+                <Routes>
+                  <Route path='/' element={<NotebookListContainer/>}/>
+                  <Route path='/notebooks' element={<NotebookListContainer/>}/>
+                  <Route path='/notebooks/:notebookId/notes' element={<NoteListContainer/>}/>
+                  <Route path='/notebooks/:notebookId/notes/:noteId' element={<NoteEditorContainer/>}/>
+                  <Route path='/login' element={<Login/>}/>
+                </Routes>
+              </Content>
+            </Layout>
+          </Layout>
+        </Layout>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  initialized: state.app.initialized,
+})
+
+const AppContainer = compose(
+    connect(mapStateToProps, {initializeApp}))
+(App);
+
+const AppMain = () => {
+  return <Provider store={store}>
+    <BrowserRouter>
+      <AppContainer/>
+    </BrowserRouter>
+  </Provider>
+}
+
+export default AppMain

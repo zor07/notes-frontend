207dcc61345621e2914bdaaae3d2d693225354c8
zor07 <anzor.karmov@gmail.com>
2022-10-22 20:39:34 +0300

    Icons.tsx

---
+++ src/components/Editor/Toolbar/Controls/Icons.tsx
@@ -0,0 +1,405 @@
+import React from "react";
+
+export const H1 = () => {
+    return (
+        <>
+            <svg stroke="currentColor"
+                 fill="currentColor"
+                 strokeWidth="0"
+                 name="h1"
+                 height="1em"
+                 width="1em"
+                 xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 24 24">
+                <path fill="none" d="M0 0H24V24H0z"/>
+                <path d="M13 20h-2v-7H4v7H2V4h2v7h7V4h2v16zm8-12v12h-2v-9.796l-2 .536V8.67L19.5 8H21z"/>
+            </svg>
+        </>
+    )
+}
+
+export const H2 = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="h2"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0H24V24H0z"/>
+            <path
+                d="M4 4v7h7V4h2v16h-2v-7H4v7H2V4h2zm14.5 4c2.071 0 3.75 1.679 3.75 3.75 0 .857-.288 1.648-.772 2.28l-.148.18L18.034 18H22v2h-7v-1.556l4.82-5.546c.268-.307.43-.709.43-1.148 0-.966-.784-1.75-1.75-1.75-.918 0-1.671.707-1.744 1.606l-.006.144h-2C14.75 9.679 16.429 8 18.5 8z"/>
+        </svg>
+    )
+}
+
+export const H3 = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="h3"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0H24V24H0z"/>
+            <path
+                d="M22 8l-.002 2-2.505 2.883c1.59.435 2.757 1.89 2.757 3.617 0 2.071-1.679 3.75-3.75 3.75-1.826 0-3.347-1.305-3.682-3.033l1.964-.382c.156.806.866 1.415 1.718 1.415.966 0 1.75-.784 1.75-1.75s-.784-1.75-1.75-1.75c-.286 0-.556.069-.794.19l-1.307-1.547L19.35 10H15V8h7zM4 4v7h7V4h2v16h-2v-7H4v7H2V4h2z"/>
+        </svg>
+    )
+}
+
+export const H4 = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="h4"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0H24V24H0z"/>
+            <path d="M13 20h-2v-7H4v7H2V4h2v7h7V4h2v16zm9-12v8h1.5v2H22v2h-2v-2h-5.5v-1.34l5-8.66H22zm-2 3.133L17.19 16H20v-4.867z"/>
+        </svg>
+    )
+}
+
+export const H5 = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="h5"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0H24V24H0z"/>
+            <path
+                d="M22 8v2h-4.323l-.464 2.636c.33-.089.678-.136 1.037-.136 2.21 0 4 1.79 4 4s-1.79 4-4 4c-1.827 0-3.367-1.224-3.846-2.897l1.923-.551c.24.836 1.01 1.448 1.923 1.448 1.105 0 2-.895 2-2s-.895-2-2-2c-.63 0-1.193.292-1.56.748l-1.81-.904L16 8h6zM4 4v7h7V4h2v16h-2v-7H4v7H2V4h2z"/>
+        </svg>
+    )
+}
+
+export const H6 = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="h6"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0H24V24H0z"/>
+            <path d="M21.097 8l-2.598 4.5c2.21 0 4.001 1.79 4.001 4s-1.79 4-4 4-4-1.79-4-4c0-.736.199-1.426.546-2.019L18.788 8h2.309zM4 4v7h7V4h2v16h-2v-7H4v7H2V4h2zm14.5 10.5c-1.105 0-2 .895-2 2s.895 2 2 2 2-.895 2-2-.895-2-2-2z"/>
+        </svg>
+    )
+}
+
+export const UndoIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="arrowGoBackFill"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path d="M8 7v4L2 6l6-5v4h5a8 8 0 1 1 0 16H4v-2h9a6 6 0 1 0 0-12H8z"/>
+        </svg>
+    )
+}
+
+export const RedoIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="arrowGoForwardFill"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path d="M16 7h-5a6 6 0 1 0 0 12h9v2h-9a8 8 0 1 1 0-16h5V1l6 5-6 5V7z"/>
+        </svg>
+    )
+}
+
+export const BoldIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="bold"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path d="M8 11h4.5a2.5 2.5 0 1 0 0-5H8v5zm10 4.5a4.5 4.5 0 0 1-4.5 4.5H6V4h6.5a4.5 4.5 0 0 1 3.256 7.606A4.498 4.498 0 0 1 18 15.5zM8 13v5h5.5a2.5 2.5 0 1 0 0-5H8z"/>
+        </svg>
+    )
+}
+
+export const ItalicIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="italic"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path d="M15 20H7v-2h2.927l2.116-12H9V4h8v2h-2.927l-2.116 12H15z"/>
+        </svg>
+    )
+}
+
+export const UnderlineIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="underline"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path d="M8 3v9a4 4 0 1 0 8 0V3h2v9a6 6 0 1 1-12 0V3h2zM4 20h16v2H4v-2z"/>
+        </svg>
+    )
+}
+
+export const StrikethroughIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="strikethrough"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path
+                d="M17.154 14c.23.516.346 1.09.346 1.72 0 1.342-.524 2.392-1.571 3.147C14.88 19.622 13.433 20 11.586 20c-1.64 0-3.263-.381-4.87-1.144V16.6c1.52.877 3.075 1.316 4.666 1.316 2.551 0 3.83-.732 3.839-2.197a2.21 2.21 0 0 0-.648-1.603l-.12-.117H3v-2h18v2h-3.846zm-4.078-3H7.629a4.086 4.086 0 0 1-.481-.522C6.716 9.92 6.5 9.246 6.5 8.452c0-1.236.466-2.287 1.397-3.153C8.83 4.433 10.271 4 12.222 4c1.471 0 2.879.328 4.222.984v2.152c-1.2-.687-2.515-1.03-3.946-1.03-2.48 0-3.719.782-3.719 2.346 0 .42.218.786.654 1.099.436.313.974.562 1.613.75.62.18 1.297.414 2.03.699z"/>
+        </svg>
+    )
+}
+
+export const BulletListIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="listUnordered"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path d="M8 4h13v2H8V4zM4.5 6.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 7a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 6.9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM8 11h13v2H8v-2zm0 7h13v2H8v-2z"/>
+        </svg>
+    )
+}
+
+export const OrderedListIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="listOrdered"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path d="M8 4h13v2H8V4zM5 3v3h1v1H3V6h1V4H3V3h2zM3 14v-2.5h2V11H3v-1h3v2.5H4v.5h2v1H3zm2 5.5H3v-1h2V18H3v-1h3v4H3v-1h2v-.5zM8 11h13v2H8v-2zm0 7h13v2H8v-2z"/>
+        </svg>
+    )
+}
+
+export const TaskedListIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="listCheck"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path d="M8 4h13v2H8V4zm-5-.5h3v3H3v-3zm0 7h3v3H3v-3zm0 7h3v3H3v-3zM8 11h13v2H8v-2zm0 7h13v2H8v-2z"/>
+        </svg>
+    )
+}
+
+export const TableIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="table2"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none" d="M0 0h24v24H0z"/>
+            <path fillRule="nonzero" d="M13 10v4h6v-4h-6zm-2 0H5v4h6v-4zm2 9h6v-3h-6v3zm-2 0v-3H5v3h6zm2-14v3h6V5h-6zm-2 0H5v3h6V5zM4 3h16a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1z"/>
+        </svg>
+    )
+}
+
+export const AlignCenterIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="table2"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <g>
+                <path fill="none" d="M0 0h24v24H0z"/>
+                <path d="M3 4h18v2H3V4zm2 15h14v2H5v-2zm-2-5h18v2H3v-2zm2-5h14v2H5V9z"/>
+            </g>
+        </svg>
+    )
+}
+
+export const AlignLeftIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="table2"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <g>
+                <path fill="none" d="M0 0h24v24H0z"/>
+                <path d="M3 4h18v2H3V4zm0 15h14v2H3v-2zm0-5h18v2H3v-2zm0-5h14v2H3V9z"/>
+            </g>
+        </svg>
+    )
+}
+
+export const AlignRightIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="table2"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <g>
+                <path fill="none" d="M0 0h24v24H0z"/>
+                <path d="M3 4h18v2H3V4zm4 15h14v2H7v-2zm-4-5h18v2H3v-2zm4-5h14v2H7V9z"/>
+            </g>
+        </svg>
+    )
+}
+
+export const AlignJustifyIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="table2"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <g>
+                <path fill="none" d="M0 0h24v24H0z"/>
+                <path d="M3 4h18v2H3V4zm0 15h18v2H3v-2zm0-5h18v2H3v-2zm0-5h18v2H3V9z"/>
+            </g>
+        </svg>
+    )
+}
+
+export const VideoLineIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="table2"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <g>
+                <path fill="none" d="M0 0h24v24H0z"/>
+                <path d="M3 3.993C3 3.445 3.445 3 3.993 3h16.014c.548 0 .993.445.993.993v16.014a.994.994 0 0 1-.993.993H3.993A.994.994 0 0 1 3 20.007V3.993zM5 5v14h14V5H5zm5.622 3.415l4.879 3.252a.4.4 0 0 1 0 .666l-4.88 3.252a.4.4 0 0 1-.621-.332V8.747a.4.4 0 0 1 .622-.332z"/>
+            </g>
+        </svg>
+    )
+}
+
+export const AddImageLineIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="table2"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <g>
+                <path fill="none" d="M0 0h24v24H0z"/>
+                <path d="M21 15v3h3v2h-3v3h-2v-3h-3v-2h3v-3h2zm.008-12c.548 0 .992.445.992.993V13h-2V5H4v13.999L14 9l3 3v2.829l-3-3L6.827 19H14v2H2.992A.993.993 0 0 1 2 20.007V3.993A1 1 0 0 1 2.992 3h18.016zM8 7a2 2 0 1 1 0 4 2 2 0 0 1 0-4z"/>
+            </g>
+        </svg>
+    )
+}
+
+export const ArrowUpIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="table2"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none"
+                  d="M0 0h24v24H0z"/>
+            <path d="M12 10.828l-4.95 4.95-1.414-1.414L12 8l6.364 6.364-1.414 1.414z"/>
+        </svg>
+    )
+}
+
+export const ArrowDownIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="table2"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none"
+                  d="M0 0h24v24H0z"/>
+            <path d="M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z"/>
+        </svg>
+    )
+}
